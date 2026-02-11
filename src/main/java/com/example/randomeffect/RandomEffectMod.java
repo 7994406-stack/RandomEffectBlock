bfa10952650d2743cefc1a41dbafbc886634265a
7994406-stack <7994406@gmail.com>
2026-02-12 02:20:40 +0300

    Fix for 1.20.1 API changes

--- src/main/java/com/example/randomeffect/RandomEffectMod.java
+++ src/main/java/com/example/randomeffect/RandomEffectMod.java
@@ -2,11 +2,12 @@ package com.example.randomeffect;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -25,12 +26,12 @@ public class RandomEffectMod {
         DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
     
     public static final RegistryObject<Block> EFFECT_BLOCK = BLOCKS.register("effect_block",
-        () -> new EffectBlock(BlockBehaviour.Properties.of(Material.STONE)
+        () -> new EffectBlock(BlockBehaviour.Properties.copy(Blocks.AMETHYST_BLOCK)
             .strength(3.0F, 3.0F)
             .requiresCorrectToolForDrops()));
     
     public static final RegistryObject<Item> EFFECT_BLOCK_ITEM = ITEMS.register("effect_block",
-        () -> new BlockItem(EFFECT_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
+        () -> new BlockItem(EFFECT_BLOCK.get(), new Item.Properties()));
     
     public RandomEffectMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -39,6 +40,14 @@ public class RandomEffectMod {
         ITEMS.register(modEventBus);
         ModBlockEntities.BLOCK_ENTITIES.register(modEventBus);
         
+        modEventBus.addListener(this::addCreative);
+        
         MinecraftForge.EVENT_BUS.register(this);
+    }
+    
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(EFFECT_BLOCK_ITEM);
+        }
     }
 }
